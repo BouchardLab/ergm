1ad72f6a0b7b30e22d4b344f78f5c9490f65b10f
Yao Xu <yaoyuanxu@berkeley.edu>
2023-05-24 12:39:08 -0700

    debugging ooou changestat C src code

--- src/changestats.c
+++ src/changestats.c
@@ -27,7 +27,7 @@ C_CHANGESTAT_FN(c_ooou) {
     /* *** don't forget tail -> head */    
     edgemult = edgestate ? -1.0 : 1.0;
     change = 0;
-    printf("edgestate: ", edgestate)
+    printf("edgestate: ", edgestate);
 
     if (!IS_OUTEDGE(head, tail)) {
         STEP_THROUGH_OUTEDGES(tail, e, node3) { 
